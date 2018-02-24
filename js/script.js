fa44c6980a997782edd47d391c9cb3f21d0511e0
mnap <learn@10g.pl>
2018-02-24 12:14:33 +0100

    Add scripts and update package.json

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 // script.js
 
-var name = prompt('Enter your name');
-// turn off popup window
-// alert('Hello, ' + name);
-/*
- * leave console printing
- */
-console.log('Hello, ' + name);
+var a = prompt('Enter triangle base:'),
+    h = prompt('Enter triangle height'),
+    triangleArea = a * h / 2;
+
+console.log('Triangle field with base a: ' + a +
+    ' and height h: ' + h + ' is equal to: ' + triangleArea + '.');
+alert('Triangle field with base a: ' + a +
+    ' and height h: ' + h + ' is equal to: ' + triangleArea + '.');
+document.write('Triangle field with base a: ' + a +
+    ' and height h: ' + h + ' is equal to: ' + triangleArea + '.');
